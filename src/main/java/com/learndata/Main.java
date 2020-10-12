abd74b7f64401b12ecdf4f07313c20105ad6d5e6
chenzhuo <chenzhuo@ybm.100.com>
2020-10-12 22:55:38 +0800

    init

---
+++ src/main/java/com/learndata/Main.java
@@ -0,0 +1,13 @@
+package com.learndata;
+
+/**
+ * @author 一方通行
+ * @title: Main
+ * @projectName LearnDataStructure
+ * @date 2020/10/12 22:54
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello word");
+    }
+}
